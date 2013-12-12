3a1e43d6e8cd0f7824a3053b614227306f8da1c6
Harry Sanjaya <Wie_harry@yahoo.co.id>
2013-12-12 22:40:06 +0700

    Create bmi.java

---
+++ bmi.java
@@ -0,0 +1,37 @@
+package com.example.bmipro2;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+
+
+public class bmi extends Activity {
+
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.kalori);
+
+ 
+ 
+  final EditText umur = (EditText) findViewById(R.id.editText1);
+  final EditText tinggi = (EditText) findViewById(R.id.editText2);
+  final EditText berat = (EditText) findViewById(R.id.editText3);
+  final TextView viewHasil = (TextView) findViewById(R.id.textView8);
+  Button tambah = (Button) findViewById(R.id.button2);
+  tambah.setOnClickListener(new OnClickListener() {
+
+   }
+  });
+ 
+ }
+}
