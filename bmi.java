3145e015fc59d681a775e68c8863f5659962c3a3
Harry Sanjaya <Wie_harry@yahoo.co.id>
2013-12-14 14:51:15 +0700

    Update bmi.java

--- bmi.java
+++ bmi.java
@@ -17,8 +17,8 @@ import android.widget.TextView;
 public class bmi extends Activity implements OnClickListener{
 	 	
 	
-	RadioButton ra0;
-	RadioButton ra1;
+	RadioButton rala;
+	RadioButton rapr;
 	Button but1;
 	Button but2;
 	EditText tinggi;
@@ -33,8 +33,8 @@ public class bmi extends Activity implements OnClickListener{
   
 	
 		
-	ra0 = (RadioButton) findViewById(R.id.radio0);
-	ra1 = (RadioButton) findViewById(R.id.radio1);
+	rala = (RadioButton) findViewById(R.id.radiolaki);
+	rapr = (RadioButton) findViewById(R.id.radiopr);
 	but2 = (Button) findViewById(R.id.button2);
 	but1 = (Button) findViewById(R.id.button1);
 	umur = (EditText) findViewById(R.id.editText1);
@@ -42,48 +42,37 @@ public class bmi extends Activity implements OnClickListener{
 	berat = (EditText) findViewById(R.id.editText3);
 	viewhasil = (TextView) findViewById(R.id.textView8);
 	
-	ra1.setOnClickListener(this);
-	ra0.setOnClickListener(this);
+	rala.setOnClickListener(this);
+	rapr.setOnClickListener(this);
 	but1.setOnClickListener(this);
 	but2.setOnClickListener(this);
 	}
 	
 	
-
 	@Override
 	public void onClick(View v){
-		if (v == but2){
-			but2.setOnClickListener(new OnClickListener() {
-
-				   @Override
-				public void onClick(View arg0) {
+		if (v == but1) {
+			umur.setText("");
+			tinggi.setText("");
+			berat.setText("");
+		}
+		
+		else if (v == but2){
+			if  (rala.isChecked()){
 				int umr = Integer.parseInt(umur.getText().toString());
 				int tng = Integer.parseInt(tinggi.getText().toString());
 				int brt = Integer.parseInt(berat.getText().toString());
-
-				int hasil = umr + tng+ brt;
+				int hasil = umr * tng * brt;
 				viewhasil.setText(String.valueOf(hasil));
-				}
-			});
+			}
+			else if (rapr.isChecked()){
+				int umr = Integer.parseInt(umur.getText().toString());
+				int tng = Integer.parseInt(tinggi.getText().toString());
+				int brt = Integer.parseInt(berat.getText().toString());
+				int hasil = umr + tng + brt;
+				viewhasil.setText(String.valueOf(hasil));
+		   	}
 		}
+		
 	}
 }
-
-//final EditText umur = (EditText) findViewById(R.id.editText1);
-//final EditText tinggi = (EditText) findViewById(R.id.editText2);
-//final EditText berat = (EditText) findViewById(R.id.editText3);
-//final TextView viewHasil = (TextView) findViewById(R.id.textView8);
-// Button tambah = (Button) findViewById(R.id.button2);
-// tambah.setOnClickListener(new OnClickListener() {
-
-/*   @Override
-public void onClick(View arg0) {
-int umr = Integer.parseInt(umur.getText().toString());
-int tng = Integer.parseInt(tinggi.getText().toString());
-int brt = Integer.parseInt(berat.getText().toString());
-
-int hasil = umr + tng+ brt;
-viewHasil.setText(String.valueOf(hasil));
-}
-});
-*/
